1f0f3fd8b9fcb0b5fc85e7a734e9cf869eaf478d
Joseph Garro <joe_garro@att.net>
2022-04-26 12:08:47 -0400

    Add encrypted packet

--- main.py
+++ main.py
@@ -9,19 +9,20 @@ from os.path import exists                  # check that files exist
 # other impots
 import json                                 # json file support + associated functions                               
 import shlex                                # command parsing to preserve substrings
+import time                                 # get time that message was created for indexing
 
 #######################################################################################################################################################
 
 # supported instructions:
 #   h                                                           = explanation + show commands
 #   m [ip:optional port] [message type] ['message'] ['key']     = send message
-#   l                                                           = list messages
+#   l [type]                                                    = list messages
 #   v [index] [destination] [name] [decryption key]             = decrypt a message'
 
 #######################################################################################################################################################
 
 # recognized commands
-VALIDCMDS = ['m', 'msg', 'v', 'view', 's', 'settings', 'h', 'help', '?', 'q', 'quit', 'e', 'exit', 'l', 'list']
+VALIDCMDS = ['m', 'msg', 'v', 'view', 'h', 'help', '?', 'q', 'quit', 'e', 'exit', 'l', 'list']
 
 # type of content that can be sent
 # -f = file; specify a file path in the next parameter
@@ -177,6 +178,19 @@ name = userInfo['name']
 currentPort = userInfo['defaultPort']
 DFTKEY = userInfo["defaultKey"]
 
+# lists to store message information
+# list for sent messages
+sentMessages = []
+
+# list for recieved messages
+receivedMessages = []
+
+# list for sent acks
+sentACK = []
+
+# list for receievd acks
+receivedACK = []
+
 # display current public IP and defined port
 print('##########################################')
 print('welcome ' + name + '!')
@@ -194,123 +208,155 @@ while(runFlag):
     # check to see if listening socket has anything 
     inputstring = str(input(name + "> "))
 
-    # split input at spaces to get command + arguments
-    commandParts = shlex.split(inputstring)
-    #print(commandParts)
 
-    # if valid command proceed
-    if(commandParts[0].lower() not in VALIDCMDS):
-        print(commandParts[0].lower(), " is not a recognized command")
+    if(inputstring != NULL and inputstring.isspace() == False):
+        # split input at spaces to get command + arguments
+        commandParts = shlex.split(inputstring)
+        #print(commandParts)
 
-    # if valid command, execute command
-    # msg [m, msg]
-    if((commandParts[0].lower() == VALIDCMDS[0]) or (commandParts[0].lower() == VALIDCMDS[1])):
-        # msg needs 4 or 5 arguments; stop processing if not met
-        numArg = length_hint(commandParts)
-        if(numArg > 5 or numArg < 4):
-            print(inputstring + ' is not a valid "m" command')
-            continue;
+        # if valid command proceed
+        if(commandParts[0].lower() not in VALIDCMDS):
+            print(commandParts[0].lower(), " is not a recognized command")
 
-        # break up destination information
-        destInfo = str(commandParts[1]).split(':')
-        ipArgs = length_hint(destInfo)
-
-        # stop if bad input
-        if(ipArgs > 2):
-            print('Invalid IP:Port combination.')
-            continue;
-
-        # stop if invalid IP address
-        if(not ipCheck(str(destInfo[0]))):
-            print(destInfo[0] + ' is not a valid IP address')
-            continue;
-
-        # assign destination IP
-        destIP = destInfo[0]
-        destPort = DFTPORT
-
-        # if valid, see if there is a port and check if it is valid
-        if(ipArgs == 2):
-            if(not portCheck(str(destInfo[1]))):
-                print(destInfo[1] + ' is not a valid port number')
+        # if valid command, execute command
+        # msg [m, msg]
+        if((commandParts[0].lower() == VALIDCMDS[0]) or (commandParts[0].lower() == VALIDCMDS[1])):
+            # msg needs 4 or 5 arguments; stop processing if not met
+            numArg = length_hint(commandParts)
+            if(numArg > 5 or numArg < 4):
+                print(inputstring + ' is not a valid "m" command')
                 continue;
 
-            # if this passes, destPort = destInfo[1]
-            destPort = str(destInfo[1])
+            # break up destination information
+            destInfo = str(commandParts[1]).split(':')
+            ipArgs = length_hint(destInfo)
 
-        # check type of message
-        msgFmt = commandParts[2].lower()
-        if(msgFmt not in VALFMT):
-            print(commandParts[2] + "is not a valid argument for the type of content to send")
-            continue;
-
-        # handle -t = text
-        fileExt = NULL
-        dataToSend = NULL
-        if(msgFmt == '-t'):
-            print('text input')
-            # update contents
-            dataToSend = commandParts[3]
-
-        if(msgFmt == '-f'):
-            print('fileInput')
-            # check that file exists
-            if(exists(commandParts[3]) == False):
-                print('Specified file does not exist!')
+            # stop if bad input
+            if(ipArgs > 2):
+                print('Invalid IP:Port combination.')
                 continue;
-            # if the file exists
-            else:
-                fileExt = getExt(commandParts[3])
-                if(fileExt == NULL):
-                    print("Can not determine file extension from the provided path")
+
+            # stop if invalid IP address
+            if(not ipCheck(str(destInfo[0]))):
+                print(destInfo[0] + ' is not a valid IP address')
+                continue;
+
+            # assign destination IP
+            destIP = destInfo[0]
+            destPort = DFTPORT
+
+            # if valid, see if there is a port and check if it is valid
+            if(ipArgs == 2):
+                if(not portCheck(str(destInfo[1]))):
+                    print(destInfo[1] + ' is not a valid port number')
                     continue;
-                fileInput = open(commandParts[3], 'rb')
-                dataToSend = fileInput.read()
 
-        # key check
-        currentKey = NULL
+                # if this passes, destPort = destInfo[1]
+                destPort = str(destInfo[1])
 
-        # if no key provided, use default key
-        if(numArg == 4):
-            currentKey = str(DFTKEY)
-
-        # if a key is provided, ensure that it is correct
-        if(numArg == 5):
-            # check that provided key is not whitespace
-            if(str(commandParts[4]).isspace()):
-                print("Provided key can not be only whitespace")
+            # check type of message
+            msgFmt = commandParts[2].lower()
+            if(msgFmt not in VALFMT):
+                print(commandParts[2] + "is not a valid argument for the type of content to send")
                 continue;
 
-            # check that provided key field is not empty
-            if(str(commandParts[4]) == ""):
-                print("Provided key can not be empty")
-                continue;
+            # handle -t = text
+            fileExt = NULL
+            dataToSend = NULL
+            if(msgFmt == '-t'):
+                print('text input')
+                # update contents
+                dataToSend = commandParts[3]
 
-        # if we are here, can open a socket and send the message
-        # all required info is here
-        print("destination IP: " + str(destIP) + "\ndestination port: " + str(destPort) + "\ndata to send: " + str(dataToSend) + "\nfile ext (if applicable): " + str(fileExt))
+            if(msgFmt == '-f'):
+                print('fileInput')
+                # check that file exists
+                if(exists(commandParts[3]) == False):
+                    print('Specified file does not exist!')
+                    continue;
+                # if the file exists
+                else:
+                    fileExt = getExt(commandParts[3])
+                    if(fileExt == NULL):
+                        print("Can not determine file extension from the provided path")
+                        continue;
+                    fileInput = open(commandParts[3], 'rb')
+                    dataToSend = fileInput.read()
 
-    # list [l, list]
-    if((commandParts[0].lower() == VALIDCMDS[13]) or (commandParts[0].lower() == VALIDCMDS[14])):
-        print('list all messages')
-        # display indexed list of all IPs that a message was received from (maybe date/time and name too)?
+            # key check
+            currentKey = NULL
 
-    # view [v, view]
-    if((commandParts[0].lower() == VALIDCMDS[2]) or (commandParts[0].lower() == VALIDCMDS[3])):
-        print('view a specific message')
-        # decrypt a message by its index
+            # if no key provided, use default key
+            if(numArg == 4):
+                currentKey = str(DFTKEY)
 
-    # settings [settings, s]
-    if((commandParts[0].lower() == VALIDCMDS[4]) or (commandParts[0].lower() == VALIDCMDS[5])):
-        print('change settings')
+            # if a key is provided, ensure that it is correct
+            if(numArg == 5):
+                # check that provided key is not whitespace
+                if(str(commandParts[4]).isspace()):
+                    print("Provided key can not be only whitespace")
+                    continue;
 
-    # help [h, help, q]
-    if((commandParts[0].lower() == VALIDCMDS[6]) or (commandParts[0].lower() == VALIDCMDS[7]) or (commandParts[0].lower() == VALIDCMDS[8])):
-        printHelp()
+                # check that provided key field is not empty
+                if(str(commandParts[4]) == ""):
+                    print("Provided key can not be empty")
+                    continue;
 
-    # quit [q, quit, e, exit]
-    if((commandParts[0].lower() == VALIDCMDS[9]) or (commandParts[0].lower() == VALIDCMDS[10]) or (commandParts[0].lower() == VALIDCMDS[11]) or (commandParts[0].lower() == VALIDCMDS[12])):
-        # kill the program
-        runFlag = False
+                # if valid, use entered key
+                currentKey = str(commandParts[4])
+
+            #print("current port: " + str(currentPort) +  "\ndestination IP: " + str(destIP) + "\ndestination port: " + str(destPort) + "\ndata to send: " + str(dataToSend) + "\nfile ext (if applicable): " + str(fileExt))
+            sendTime = int(time.time())
+
+            # if we are here, can open a socket and send the message
+            # all required info is here
+
+            # encrypt data here
+
+            # encrypt key here
+
+            # create dict here
+            encrPacket = {
+                "timeSent" : sendTime,
+                "responseIP" : "NULL",
+                "responsePort" : currentPort,
+                "destinationIP" : destIP,
+                "destinationPort" : destPort,
+                "encryptedMessage": dataToSend,                 # TODO replace with encrypted data
+                "encryptedKey" : currentKey,                    # TODO replace with encrypted key
+                "dataType": fileExt
+            }
+
+            # store time sent, dest IP, despPort, and encryptedKey
+            sentMessages.append({encrPacket['timeSent'], encrPacket['destinationIP'], encrPacket['destinationPort'], encrPacket['encryptedKey']})
+            #print(sentMessages)
+            
+            # send with socket here
+
+        # list [l, list]
+        if((commandParts[0].lower() == VALIDCMDS[11]) or (commandParts[0].lower() == VALIDCMDS[12])):
+            print('list all messages')
+            # display indexed list of all IPs that a message was sent to or received from (maybe date/time and name too)?
+
+        # view [v, view]
+        if((commandParts[0].lower() == VALIDCMDS[2]) or (commandParts[0].lower() == VALIDCMDS[3])):
+            print('view a specific message')
+            # decrypt a message by its index
+            # remove dict with matching time index - https://www.geeksforgeeks.org/python-removing-dictionary-from-list-of-dictionaries/
+            #sentMessages = [i for i in sentMessages if not (i['timeSent'] == sendTime)]
+            print(sentMessages)
+
+        # settings [settings, s]
+        #if((commandParts[0].lower() == VALIDCMDS[4]) or (commandParts[0].lower() == VALIDCMDS[5])):
+        #    print('change settings')
+
+        # help [h, help, q]
+        if((commandParts[0].lower() == VALIDCMDS[4]) or (commandParts[0].lower() == VALIDCMDS[5]) or (commandParts[0].lower() == VALIDCMDS[6])):
+            printHelp()
+
+        # quit [q, quit, e, exit]
+        if((commandParts[0].lower() == VALIDCMDS[7]) or (commandParts[0].lower() == VALIDCMDS[8]) or (commandParts[0].lower() == VALIDCMDS[8]) or (commandParts[0].lower() == VALIDCMDS[10])):
+            # kill the program
+            runFlag = False
 
 # kill all sockets/threads after this point
