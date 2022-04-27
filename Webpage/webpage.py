1685e5a5301be2f60f925dc8276fa58938886311
Nate Schroeder <nes39@uakron.edu>
2022-04-26 22:32:21 -0400

    Website running with initial HTML.

--- Webpage/webpage.py
+++ Webpage/webpage.py
@@ -1,6 +1,14 @@
-from app import app
-from flask import render_template
+from flask import render_template, Flask, request
+
+app = Flask(__name__, template_folder='template')
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/new-message')
+def new_message():
+    return render_template('new_message.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
